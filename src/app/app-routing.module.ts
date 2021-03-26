89ac0258eda3b62a88a82c21e26ab27a5284c828
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-03-26 00:03:08 -0500

    Main/<Logup>: creado el componente del layout para renderizar solo los hijos. A crear el login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormLayoutComponent } from './form-layout/form-layout.component';
 
 const routes: Routes = [
-  {
-    path: 'logup',
-    loadChildren: () => import('./pages/logup/logup.module').then(m => m.LogupModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
-  },
+  { path: '', component: FormLayoutComponent,
+    children: [
+      {
+        path: 'logup',
+        loadChildren: () => import('./pages/logup/logup.module').then(m => m.LogupModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormLayoutComponent } from './form-layout/form-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLayoutComponent
   ],
   imports: [
     BrowserModule,
