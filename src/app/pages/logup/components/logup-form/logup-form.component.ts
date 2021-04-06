07a85edc30e30128be519d4f657774c71fa8f593
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-04-05 22:58:45 -0500

    Master/<Logup/>: validaciones hechas, a ver ahora los del display de los errores y de las expresiones regulares.

--- src/app/pages/logup/components/logup-form/logup-form.component.ts
+++ src/app/pages/logup/components/logup-form/logup-form.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-logup-form',
   templateUrl: './logup-form.component.html',
-  styleUrls: [
-    '../../../form.shared.scss'
-  ]})
-
+  styleUrls: ['../../../form.shared.scss']})
 
 export class LogupFormComponent {
+  
   showpwd = false;
+
+  logupForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  };
+
+  buildForm() {
+    let { required, minLength, maxLength, email } = Validators;
+    this.logupForm = this.formBuilder.group({
+      name: ['', [required, minLength(10), maxLength(30)]],
+      username: ['', [required, minLength(8), maxLength(12)]],
+      email: ['', [email]],
+      password: ['', [required, minLength(8)]],
+    });
+  };
+
+
 };
--- src/app/pages/logup/logup.module.ts
+++ src/app/pages/logup/logup.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogupFormComponent } from './components/logup-form/logup-form.component';
 import { LogupRoutingModule } from './logup.routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { LogupRoutingModule } from './logup.routing.module';
   ],
   imports: [
     CommonModule,
-    LogupRoutingModule
+    LogupRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class LogupModule {};
