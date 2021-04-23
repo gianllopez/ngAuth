8d5f5a375285ff481da57d8c4f090629e0d0412b
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-04-23 16:44:36 -0500

    Master:<login-form/>: a arreglar el error de routing transitions.

--- src/app/pages/login/components/login-form/login-form.component.ts
+++ src/app/pages/login/components/login-form/login-form.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-styleUrls: [
-  '../../../form.shared.scss'
-]})
+styleUrls: [ '../../../form.shared.scss' ]})
 export class LoginFormComponent {
-  showpwd = false;
+  
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
+  };
+
+  buildForm(): void {
+    const { required, minLength, maxLength, email } = Validators;
+    this.loginForm = this.formBuilder.group({
+      uoe: ['', [required, minLength(8), maxLength(12)]],      
+      password: ['', [required, minLength(8)]]
+    });
+  };
+
 };
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LoginFormComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    SharedModule
   ]
 })
 export class LoginModule { }
