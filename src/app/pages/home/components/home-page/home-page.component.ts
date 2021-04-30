1f1a1fd221420b2834d72f22fd4b55acada59a38
Gian Lopez <lopezarizagianlucas@gmail.com>
2021-04-29 19:02:09 -0500

    Master/feedback: agregado el footer, el loading al button del login, el button del logout a la page del home.

--- src/app/pages/home/components/home-page/home-page.component.ts
+++ src/app/pages/home/components/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/services/api/api.service';
 
 @Component({
@@ -8,9 +9,14 @@ import { ApiService } from 'src/app/shared/services/api/api.service';
   styleUrls: ['./home-page.component.scss']})
 export class HomePageComponent implements OnInit {
 
-  constructor(private api: ApiService, private title: Title) {};
+  constructor(private api: ApiService, private title: Title, private router: Router) {};
 
   name!: string;
+
+  logOut(): void {
+    localStorage.clear();
+    this.router.navigateByUrl('login');
+  };
 
   ngOnInit(): void {
     this.title.setTitle('ngAuth - Home');
--- src/app/pages/login/components/login-form/login-form.component.ts
+++ src/app/pages/login/components/login-form/login-form.component.ts
@@ -10,8 +10,8 @@ import { ApiService } from 'src/app/shared/services/api/api.service';
 export class LoginFormComponent {
   
   loginForm!: FormGroup;
-
   invalidCredentials!: string;
+  loading = false;
 
   constructor(private formBuilder: FormBuilder, private api: ApiService, private title: Title) {
     this.title.setTitle('ngAuth - Login');
@@ -28,10 +28,12 @@ export class LoginFormComponent {
   };
 
   errorHandler(errors: ValidationErrors): void {
+    this.loading = false;
     this.invalidCredentials = errors.user;
   };
 
   onSubmit(): void {
+    this.loading = true;
     this.api.submitUser('/login', this.loginForm.value, this.errorHandler);
   };
 
